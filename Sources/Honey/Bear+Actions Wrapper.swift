ac902552e41253ba16004e05fd07054d0b157f0f
Valentin Walter <valentinwalter@icloud.com>
2021-05-22 18:17:40 +0200

    Add missing actions

--- Sources/Honey/Bear+Actions Wrapper.swift
+++ Sources/Honey/Bear+Actions Wrapper.swift
@@ -13,7 +13,7 @@ import Middleman
 extension Bear {
     public typealias Handler<T> = (T) -> Void
     public typealias SuccessHandler<A> = Handler<A.Output> where A: Action
-    public typealias ErrorHandler = () -> Void
+    public typealias Closure = () -> Void
 	
     public struct Options: OptionSet {
         public var rawValue: Int
@@ -33,17 +33,17 @@ extension Bear {
     }
 }
 
-extension Bear {
+public extension Bear {
 
     //MARK:- Create
 
-    public static func create(
+	static func create(
         note: Note,
         tags: [String] = [],
         file: File? = nil,
         options: Options = [],
 		onSuccess handleSuccess: @escaping SuccessHandler<Create> = { _ in },
-		onError handleError: @escaping ErrorHandler = { }
+		onError handleError: @escaping Closure = { }
     ) {
         Bear().run(
             action: Create(),
@@ -75,7 +75,7 @@ extension Bear {
         )
     }
 	
-	public static func create(
+	static func create(
 		note: Note,
 		tags: [String] = [],
 		file: File? = nil,
@@ -95,12 +95,12 @@ extension Bear {
 
     //MARK:- Open
 
-    public static func open(
+	static func open(
         note: Note.Lookup,
         at header: String? = nil,
         options: Options = [],
 		onSuccess handleSuccess: @escaping SuccessHandler<OpenNote> = { _ in },
-        onError handleError: @escaping ErrorHandler = { }
+        onError handleError: @escaping Closure = { }
     ) {
         Bear().run(
             action: OpenNote(),
@@ -131,7 +131,7 @@ extension Bear {
         )
     }
 
-	public static func open(
+	static func open(
 		note: Note.Lookup,
 		at header: String? = nil,
 		options: Options = [],
@@ -149,7 +149,7 @@ extension Bear {
 
     //MARK:- Read
 
-    public static func read(
+	static func read(
         note lookup: Note.Lookup,
         excludeTrashed: Bool = false,
         then handler: @escaping Handler<Note>
@@ -183,7 +183,7 @@ extension Bear {
 
     //MARK:- Add Text
 
-    public static func addText(
+	static func addText(
         note lookup: Note.Lookup,
         text: String,
 		at header: String? = nil,
@@ -191,7 +191,7 @@ extension Bear {
         tags: [String] = [],
 		options: Options = [.hideNote, .hideWindow],
 		onSuccess handleSuccess: @escaping SuccessHandler<AddText> = { _ in },
-        onError handleError: @escaping ErrorHandler = { }
+        onError handleError: @escaping Closure = { }
     ) {
         Bear().run(
             action: AddText(),
@@ -225,7 +225,7 @@ extension Bear {
         )
     }
 	
-	public static func addText(
+	static func addText(
 		note lookup: Note.Lookup,
 		text: String,
 		at header: String? = nil,
@@ -249,14 +249,14 @@ extension Bear {
     
     //MARK:- Add File
 
-    public static func addFile(
+	static func addFile(
         note lookup: Note.Lookup,
         file: File,
         at header: String? = nil,
         mode: AddMode,
 		options: Options = [.hideNote, .hideWindow],
 		onSuccess handleSuccess: @escaping SuccessHandler<AddFile> = { _ in },
-        onError handleError: @escaping ErrorHandler = { }
+        onError handleError: @escaping Closure = { }
     ) {
         Bear().run(
             action: AddFile(),
@@ -287,7 +287,7 @@ extension Bear {
         )
     }
 	
-	public static func addFile(
+	static func addFile(
 		note lookup: Note.Lookup,
 		file: File,
 		at header: String? = nil,
@@ -301,6 +301,816 @@ extension Bear {
 			at: header,
 			mode: mode,
 			options: options,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Get Tags
+	
+	static func getTags(
+		onSuccess handleSuccess: @escaping SuccessHandler<Tags>
+	) {
+		guard let token = token else {
+			fatalError("""
+			⛔️ `getTags` requires a token to be present.
+			ℹ️ Provide your Bear API token via `Bear.token = "..."`
+			""")
+		}
+		
+		Bear().run(
+			action: Tags(),
+			with: .init(token: token),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): break
+				case .cancel: break
+				}
+			}
+		)
+	}
+	
+	
+	//MARK:- Open Tag
+	
+	static func open(
+		tag: String,
+		onSuccess handleSuccess: @escaping SuccessHandler<OpenTag> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		guard let token = token else {
+			fatalError("""
+			⛔️ `openTag` requires a token to be present.
+			ℹ️ Provide your Bear API token via `Bear.token = "..."`
+			""")
+		}
+		
+		Bear().run(
+			action: OpenTag(),
+			with: .init(
+				name: tag,
+				token: token
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func open(
+		tag: String,
+		onSuccess handleSuccess: @escaping SuccessHandler<OpenTag>
+	) {
+		Bear.open(
+			tag: tag,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Rename Tag
+	
+	static func rename(
+		tag: String,
+		to newName: String,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping SuccessHandler<RenameTag> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: RenameTag(),
+			with: .init(
+				name: tag,
+				newName: newName,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func rename(
+		tag: String,
+		to newName: String,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping SuccessHandler<RenameTag>
+	) {
+		Bear.rename(
+			tag: tag,
+			to: newName,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Delete Tag
+	
+	static func delete(
+		tag: String,
+ 		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping SuccessHandler<DeleteTag> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: DeleteTag(),
+			with: .init(
+				name: tag,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func delete(
+		tag: String,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping SuccessHandler<DeleteTag>
+	) {
+		Bear.delete(
+			tag: tag,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Trash
+	
+	static func trash(
+		id: String,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping Closure = { },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Trash(),
+			with: .init(
+				id: id,
+				search: nil,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success: handleSuccess()
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func trash(
+		id: String,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping Closure
+	) {
+		Bear.trash(
+			id: id,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	static func searchTrash(
+		for query: String,
+		onSuccess handleSuccess: @escaping Closure = { },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Trash(),
+			with: .init(
+				id: nil,
+				search: query,
+				showWindow: false
+			),
+			then: { response in
+				switch response {
+				case .success: handleSuccess()
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func searchTrash(
+		for query: String,
+		onSuccess handleSuccess: @escaping Closure = { }
+	) {
+		Bear.searchTrash(
+			for: query,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Archive
+	
+	static func archive(
+		id: String,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping Closure = { },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Archive(),
+			with: .init(
+				id: id,
+				search: nil,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success: handleSuccess()
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func archive(
+		id: String,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping Closure
+	) {
+		Bear.archive(
+			id: id,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	static func searchArchive(
+		for query: String,
+		onSuccess handleSuccess: @escaping Closure = { },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Archive(),
+			with: .init(
+				id: nil,
+				search: query,
+				showWindow: false
+			),
+			then: { response in
+				switch response {
+				case .success: handleSuccess()
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func searchArchive(
+		for query: String,
+		onSuccess handleSuccess: @escaping Closure = { }
+	) {
+		Bear.searchArchive(
+			for: query,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Untagged
+	
+	static func allUntagged(
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Untagged> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Untagged(),
+			with: .init(
+				search: nil,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func allUntagged(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Today>
+	) {
+		Bear.allUntagged(
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	static func searchUntagged(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Untagged> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Untagged(),
+			with: .init(
+				search: query,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func searchUntagged(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Today>
+	) {
+		Bear.searchUntagged(
+			for: query,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Todo
+	
+	static func allTodos(
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Todo> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Todo(),
+			with: .init(
+				search: nil,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func allTodos(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Today>
+	) {
+		Bear.allTodos(
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	static func searchTodos(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Todo> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Todo(),
+			with: .init(
+				search: query,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func searchTodos(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Today>
+	) {
+		Bear.searchTodos(
+			for: query,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Today
+	
+	static func allToday(
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Today> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Today(),
+			with: .init(
+				search: nil,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func allToday(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Today>
+	) {
+		Bear.allToday(
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	static func searchToday(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Today> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Today(),
+			with: .init(
+				search: query,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func searchToday(
+		for query: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Today>
+	) {
+		Bear.searchToday(
+			for: query,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Locked
+
+	static func searchLocked(
+		for query: String,
+		onSuccess handleSuccess: @escaping Closure = { },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: Locked(),
+			with: .init(
+				search: query,
+				showWindow: false
+			),
+			then: { response in
+				switch response {
+				case .success: handleSuccess()
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	func searchLocked(
+		for query: String,
+		onSuccess handleSuccess: @escaping Closure
+	) {
+		Bear.searchLocked(
+			for: query,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Search
+
+	static func search(
+		for query: String,
+		in tag: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Search> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		guard let token = token else {
+			fatalError("""
+			⛔️ `search` requires a token to be present.
+			ℹ️ Provide your Bear API token via `Bear.token = "..."`
+			""")
+		}
+		
+		Bear().run(
+			action: Search(),
+			with: .init(
+				search: query,
+				tag: tag,
+				showWindow: showWindow,
+				token: token
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func search(
+		for query: String,
+		in tag: String,
+		showWindow: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<Search>
+	) {
+		Bear.search(
+			for: query,
+			in: tag,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Grab URL
+	
+	// wait: false
+	static func create(
+		from url: URL,
+		tags: [String],
+		pin: Bool = false,
+		onSuccess handleSuccess: @escaping Closure = { },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: GrabURL(),
+			with: .init(
+				url: url,
+				tags: tags,
+				pin: pin,
+				wait: false
+			),
+			then: { response in
+				switch response {
+				case .success: handleSuccess()
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func create(
+		from url: URL,
+		tags: [String],
+		pin: Bool = false,
+		onSuccess handleSuccess: @escaping Closure
+	) {
+		Bear.create(
+			from: url,
+			tags: tags,
+			pin: pin,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	// wait: true
+	static func create(
+		from url: URL,
+		tags: [String],
+		pin: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<GrabURL> = { _ in },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: GrabURL(),
+			with: .init(
+				url: url,
+				tags: tags,
+				pin: pin,
+				wait: true
+			),
+			then: { response in
+				switch response {
+				case .success(let output):
+					guard let output = output else {
+						handleError()
+						return
+					}
+					handleSuccess(output)
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func create(
+		from url: URL,
+		tags: [String],
+		pin: Bool = false,
+		onSuccess handleSuccess: @escaping SuccessHandler<GrabURL>
+	) {
+		Bear.create(
+			from: url,
+			tags: tags,
+			pin: pin,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Change Theme
+	
+	static func change(
+		theme: Theme,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping Closure = { },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: ChangeTheme(),
+			with: .init(
+				theme: theme,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success: handleSuccess()
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func change(
+		theme: Theme,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping Closure
+	) {
+		Bear.change(
+			theme: theme,
+			showWindow: showWindow,
+			onSuccess: handleSuccess,
+			onError: { }
+		)
+	}
+	
+	
+	//MARK:- Change Font
+	
+	static func change(
+		font: Font,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping Closure = { },
+		onError handleError: @escaping Closure = { }
+	) {
+		Bear().run(
+			action: ChangeFont(),
+			with: .init(
+				font: font,
+				showWindow: showWindow
+			),
+			then: { response in
+				switch response {
+				case .success: handleSuccess()
+				case .error(_, _): handleError()
+				case .cancel: handleError()
+				}
+			}
+		)
+	}
+	
+	static func change(
+		font: Font,
+		showWindow: Bool = true,
+		onSuccess handleSuccess: @escaping Closure
+	) {
+		Bear.change(
+			font: font,
+			showWindow: showWindow,
 			onSuccess: handleSuccess,
 			onError: { }
 		)
