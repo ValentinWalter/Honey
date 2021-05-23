bc34fb969f2070072687aa61ddc78e3e24888790
Valentin Walter <valentinwalter@icloud.com>
2021-05-23 17:03:48 +0200

    Use environment variable for Bear.token

--- Sources/Honey/Bear.swift
+++ Sources/Honey/Bear.swift
@@ -22,10 +22,16 @@ import Middleman
 ///
 ///
 public struct Bear: App {
-    /// Bear's API token
+    /// Your Bear API token. You can either set this directly or provide an
+	/// environment variable called `BEAR_API_TOKEN` via
+	/// `Edit scheme… > Run > Arguments > Environment Variables`.
     ///
     /// Learn more at about [API Token Generation](https://bear.app/faq/X-callback-url%20Scheme%20documentation/#token-generation).
-    public static var token: String? = nil
-	
-	public var scheme: String { "bear" }
+	public static var token: String? = {
+		if let token = ProcessInfo.processInfo.environment["BEAR_API_TOKEN"] {
+			return token
+		} else {
+			return nil
+		}
+	}()
 }
